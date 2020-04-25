95677f9531b16a15c7454f40a26dd88788b50248
rodri <rodri@192.168.239.130>
2020-04-24 22:43:56 -0300

    Teste

--- logica-variaveis/src/main/java/logica/variaveis/Main.java
+++ logica-variaveis/src/main/java/logica/variaveis/Main.java
@@ -3,8 +3,13 @@ package logica.variaveis;
 public class Main {
 
 	public static void main(String[] args) {
+		int idade = 20;
+		double nota = 8.6;
+		boolean ligado = true;
+		boolean ativo = false;
+		String nome = "Carlos";
 		
-
+		System.out.println(nome);
 	}
 
 }
